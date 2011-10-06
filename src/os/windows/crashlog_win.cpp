b332b73f1d246206ad00688e92542b789e7cff4f
Dolly <Dolly@.dolly.lan>
2011-10-06 19:34:43 +0200

    Merge sai10 - Do not show crash window on dedicated server

--- src/os/windows/crashlog_win.cpp
+++ src/os/windows/crashlog_win.cpp
@@ -25,6 +25,7 @@
 #include "../../strings_func.h"
 #include "../../gamelog.h"
 #include "../../saveload/saveload.h"
+#include "../../network/network.h"
 
 #include <windows.h>
 #include <signal.h>
@@ -526,7 +527,7 @@ static LONG WINAPI ExceptionHandler(EXCEPTION_POINTERS *ep)
 	/* Close any possible log files */
 	CloseConsoleLogIfActive();
 
-	if (_safe_esp) {
+	if (_safe_esp && !_network_dedicated) {
 #ifdef _M_AMD64
 		ep->ContextRecord->Rip = (DWORD64)ShowCrashlogWindow;
 		ep->ContextRecord->Rsp = (DWORD64)_safe_esp;
